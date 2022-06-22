9059c01e2d6f891c5d76e64f8374ef1e5dda6a31
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-06-22 06:07:50 -0300

    feat: lesson component and sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
 
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      availableAt
+      id
+      lessonType
+      slug
+      title
+    }
+  }
+`;
+
+interface GetLessonsQueryResponse {
+  lessons: Array<{
+    id: string;
+    title: string;
+    slug: string;
+    availableAt: string;
+    lessonType: "live" | "class";
+  }>;
+}
+
 export function Sidebar() {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="block text-2xl border-b border-gray-600 font-bold pb-6 mb-6">
@@ -8,12 +33,17 @@ export function Sidebar() {
       </span>
 
       <div className="flex flex-col gap-8">
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
+        {data?.lessons.map((lesson) => {
+          return (
+            <Lesson
+              key={lesson.id}
+              title={lesson.title}
+              slug={lesson.slug}
+              type={lesson.lessonType}
+              availableAt={new Date(lesson.availableAt)}
+            />
+          );
+        })}
       </div>
     </aside>
   );
